b0975596b78efcf6265b376668c89795f55307e3
Lsp <spyro9282@gmail.com>
2022-12-30 21:38:28 +0900

    [chore]Post이름 수정

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -3,10 +3,10 @@ import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
-    constructor(private readonly userService: UserService) {}
+  constructor(private readonly userService: UserService) {}
 
   @Post()
   signup(): string {
     return this.userService.getTest();
-  } 
+  }
 }

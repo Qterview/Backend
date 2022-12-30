1b260ecb1114878bc1a782fd208f452383712c75
kwon-ga <rnjsdydry@gmail.com>
2022-12-30 20:08:25 +0900

    시작 git add .

---
+++ src/app.module.ts
@@ -0,0 +1,42 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+// import { ChatGPTAPIBrowser } from 'chatgpt'
+import { UserModule } from './user/user.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import {Users} from './user/entity/user.entity'
+import { SearchModule } from './search/search.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      // envFilePath: [`${__dirname}/.env`],
+      envFilePath: [`.env`],
+    }),
+    
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        return {
+          type: 'mysql',
+          host: configService.get('DB_HOST') ,
+          port: +configService.get<number>('DB_PORT') ,
+          username: configService.get('DB_USER') ,
+          database: configService.get('DB_NAME') ,
+          password: configService.get('DB_PASSWORD') ,
+          //entities: [__dirname + '/**/*.entity{.ts,.js}'],
+          entities: [Users],
+          synchronize: false,
+        };
+      },
+    }),
+    UserModule,
+    SearchModule
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}

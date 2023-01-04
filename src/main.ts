6eb72b79891e7296b9eaf0e62cf780acf160ec27
Lsp <spyro9282@gmail.com>
2023-01-04 17:49:10 +0900

    [feat]cors,https설정 추가

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,32 @@ import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.js';
 import { MyGPT } from './util/chatgpt.js';
+import * as fs from 'fs';
 
 async function bootstrap() {
   let configService = new ConfigService();
-  const app = await NestFactory.create(AppModule);
+  const port = 3000;
+
+  if (configService.get<string>('BUILD_ENV') === 'production') {
+    const httpsOptions = {
+      key: fs.readFileSync(configService.get<string>('KEY')),
+      cert: fs.readFileSync(configService.get<string>('CERT')),
+    };
+
+    const app = await NestFactory.create(AppModule, {
+      httpsOptions,
+    });
+    app.enableCors();
+
+    await app.listen(port);
+    console.log(`HTTPS 서버가 실행되었습니다.PORT::${port}`);
+  } else {
+    const app = await NestFactory.create(AppModule);
+    app.enableCors();
+
+    await app.listen(port);
+    console.log(`HTTP 서버가 실행되었습니다.PORT::${port}`);
+  }
   const myGPT = new MyGPT();
 
   // gpt에 사용되는 api 생성
@@ -14,8 +36,6 @@ async function bootstrap() {
     configService.get<string>('OPENAI_PASSWORD'),
     configService.get<string>('NOPECHAKEY'),
   );
-
-  await app.listen(3000);
 }
 
 bootstrap();

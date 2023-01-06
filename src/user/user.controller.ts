203c8ae20d326331eeaf41af91e2d3adde39cf1a
Lsp <spyro9282@gmail.com>
2023-01-07 00:00:59 +0900

    [feat]회원가입

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,23 @@
-import { Controller, Get, Post } from '@nestjs/common';
-import { UserService } from './user.service';
+import {
+  Controller,
+  Get,
+  Post,
+  ValidationPipe,
+  Body,
+  UsePipes,
+} from '@nestjs/common';
+import { Users } from '../entities/user.entity.js';
+import { CreateUserDto } from './dto/create_user.dto.js';
+import { UserService } from './user.service.js';
 
+@UsePipes(ValidationPipe)
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  //회원가입
   @Post()
-  signup(): string {
-    return this.userService.signup();
+  signup(@Body(ValidationPipe) createUserDto: CreateUserDto): Promise<Users> {
+    return this.userService.signup(createUserDto);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Users } from '../entities/user.entity.js';
+import { CreateUserDto } from './dto/create_user.dto.js';
+import { UsersRepository } from './user.repository.js';
+import bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
+  constructor(
+    @InjectRepository(UsersRepository)
+    private usersRepository: UsersRepository,
+  ) {}
   getTest(): string {
     return 'hello!';
   }
-  signup(): any {}
+  async signup(createUserDto: CreateUserDto): Promise<Users> {
+    const { Id, password, name } = createUserDto;
+    const hashed = bcrypt.hash(password);
+
+    const user = {
+      Id,
+      password: hashed,
+      name,
+    };
+
+    return await this.usersRepository.signup(user);
+  }
 }

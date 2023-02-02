a99e917505be37938dac10649475ebee046a6c81
Lsp <spyro9282@gmail.com>
2023-02-02 23:55:42 +0900

    [feat]user모듈 mongoose모듈 import

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from '../schemas/user.schema';
 
 @Module({
-  imports: [HttpModule],
+  imports: [
+    HttpModule,
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+  ],
   controllers: [UserController],
   providers: [UserService],
 })
